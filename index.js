cb44bc8068c0b742f1febd5b45ee65a57539c974
George Yue <theothergeorge@gmail.com>
2017-01-17 23:55:15 -0800

    setup endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var _ = require('lodash');
+var fs = require('fs');
 
 /** data **/
 var events = require('./events');
@@ -11,7 +12,18 @@ function respond(req, res, next) {
 }
 
 var server = restify.createServer();
-server.use(restify.CORS());
+
+server.get('/hi', function(req, res, next) {
+  var file = fs.readFileSync('./index.html', 'utf-8');
+
+  res.writeHead(200, {
+    'Content-Length': Buffer.byteLength(file),
+    'Content-Type': 'text/html'
+  });
+  res.write(file);
+  res.end();
+
+});
 
 server.get('/orders/:id', function(req, res, next) {
   res.send( _.find(orders, {id: parseInt(req.params.id)}) );
@@ -27,9 +39,8 @@ server.get('/orders', function(req, res, next) {
   var data = orders.map(
     function(item) {
       return _.pick(item, [
-        'id', 'seller',
+        'id',
         'confirmation_number',
-        'tickets',
         'order_type',
         'delivery_id',
         'event_id'
